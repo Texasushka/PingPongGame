16acfc4a657b54a43b4ad8f7c7bb2602e2f3eedd
Vladyslav_Tykhonenko <vladtihonenko@gmail.com>
2023-07-05 00:06:51 +0200

    Base level created

---
+++ Source/PingPongGame/PingPongGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PingPongGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINGPONGGAME_API APingPongGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
